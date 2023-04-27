4b4f6c6ff506352432ae19ac82ce9a7140c55c25
lawrence-254 <lodhiambo26@gmail.com>
2023-04-27 05:21:02 -0700

    correction

--- main.c
+++ main.c
@@ -1,58 +1,132 @@
 #include "shell.h"
-#include "execmd.c"
 /**
  * main - point of access to our code
  * @ac: number of argument
  * @argv: values in argument
  * Return: 0 when successfully executed and 1 when erratic
  */
+
+void print_prompt(const char *prompt);
+ssize_t read_input(char **line_ptr, size_t *n);
+char **tokenize_input(char *line_ptr, const char *delim, int *num_tokens);
+void free_tokens(char **tokens, int num_tokens);
+void execute_command(char **tokens);
+
 int main(int ac, char **argv)
 {
-	char *prompt = "($)";
-	char *line_ptr = NULL, *line_ptr_copy = NULL, *token;
+	const char *prompt = "(ODIN $ )";
+	char *line_ptr = NULL;
 	size_t n = 0;
 	ssize_t n_value;
 	const char *delim = " \n";
-	int i, num_tokens = 0;
+	int num_tokens = 0;
 
 	(void)ac;
-while (1)
-{
-printf("%s", prompt);
-n_value = getline(&line_ptr, &n, stdin);
-if (n_value == -1)
-{
-printf("Exiting shell...\n");
-return (-1);
-}
-		line_ptr_copy = malloc(sizeof(char) * n_value);
-		if (line_ptr_copy == NULL)
+	while (1)
+	{
+		print_prompt(prompt);
+		n_value = read_input(&line_ptr, &n);
+		if (n_value == -1)
 		{
-			perror("tsh: memory allocation error");
+			printf("Exiting shell...\n");
 			return (-1);
 		}
-		strcpy(line_ptr_copy, line_ptr);
-		token = strtok(line_ptr, delim);
-		while (token != NULL)
-		{
-			num_tokens++;
-			token = strtok(NULL, delim);
-		}
-		num_tokens++;
-		argv = malloc(sizeof(char *) * num_tokens);
-		token = strtok(line_ptr_copy, delim);
-
-		for (i = 0; token != NULL; i++)
-		{
-			argv[i] = malloc(sizeof(char) * strlen(token));
-			strcpy(argv[i], token);
-			token = strtok(NULL, delim);
-		}
-		argv[i] = NULL;
-		execmd(argv);
+		argv = tokenize_input(line_ptr, delim, &num_tokens);
+		execute_command(argv);
+		free_tokens(argv, num_tokens);
+		free(line_ptr);
+		line_ptr = NULL;
 	}
-	free(line_ptr_copy);
-	free(line_ptr);
-
 	return (0);
 }
+/**
+ * print_promp - prints prompter when shell is initialised
+ * @prompt: the prompt to be printed
+ * Return: void
+ */
+void print_prompt(const char *prompt)
+{
+	printf("%s", prompt);
+}
+/**
+ * read_input - reads what the user types
+ * @line_ptr:  the line to be read
+ * @n: size of line
+ * Return: string
+ */
+ssize_t read_input(char **line_ptr, size_t *n)
+{
+	return (getline(line_ptr, n, stdin));
+}
+/**
+ * tokenize_input - converts the input into string
+ * @line_ptr:line to be read from input
+ * @delim: a delimeter
+ * @num_tokens: size of the string
+ * Return: string
+ */
+char **tokenize_input(char *line_ptr, const char *delim, int *num_tokens)
+{
+	char *line_ptr_copy, *token;
+	int i;
+	char **tokens;
+
+	line_ptr_copy = strdup(line_ptr);
+	if (line_ptr_copy == NULL)
+	{
+		perror("tsh: memory allocation error");
+		exit(-1);
+	}
+	token = strtok(line_ptr, delim);
+	while (token != NULL)
+	{
+		(*num_tokens)++;
+		token = strtok(NULL, delim);
+	}
+	(*num_tokens)++;
+	tokens = malloc(sizeof(char *) * (*num_tokens));
+	if (tokens == NULL)
+	{
+		perror("tsh: memory allocation error");
+		exit(-1);
+	}
+	token = strtok(line_ptr_copy, delim);
+	for (i = 0; token != NULL; i++)
+	{
+		tokens[i] = strdup(token);
+		if (tokens[i] == NULL)
+		{
+			perror("tsh: memory allocation error");
+			exit(-1);
+		}
+		token = strtok(NULL, delim);
+	}
+	tokens[i] = NULL;
+	free(line_ptr_copy);
+	return (tokens);
+}
+/**
+ * free_token - clears the memory
+ * @tokens: pointer to memory
+ * @num_tokens: size of memory
+ * Return: void
+ */
+void free_tokens(char **tokens, int num_tokens)
+{
+	int i;
+
+	for (i = 0; i < num_tokens; i++)
+	{
+		free(tokens[i]);
+	}
+	free(tokens);
+}
+/**
+ * execute_command - calls a command to execute function
+ * @argv: argument
+ */
+void execute_command(char **argv)
+{
+	execmd(argv);
+}
+
